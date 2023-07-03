bc7b4a562baa2e99f6afac89f435e43c6a5681ab
miLLLLLLLL <rlaalfl1996@gmail.com>
2023-07-03 10:30:40 +0900

    [ADD]:북마크 오류 수정 완료

--- src/main/resources/static/js/org/org_vol.js
+++ src/main/resources/static/js/org/org_vol.js
@@ -1,21 +1,18 @@
-	 let userId = null;
-   if (document.querySelector("user-id") != null)
-      userId = document.querySelector("user-id").value;
-      
-      // user_id가 계속 null
-      
-function volListLoad(url) {
-  	let volList = document.querySelector(".vol_list");
-	let id = document.querySelector('input[name="cid"]');
-	let idvalue = id.value;
-	
-  fetch(url)
-    //`http://localhost:8080/api/org?c=${id}`
-    .then((response) => response.json())
-    .then((list) => {
-      console.log(list);
 
-      volList.innerHTML = "";
+function volListLoad(url) {
+	let volList = document.querySelector(".vol_list");
+	let id = document.querySelector('input[name="cid"]');
+
+	fetch(url)
+		//`http://localhost:8080/api/org?c=${id}`
+		.then(response => response.json())
+		.then(list => {
+
+			volList.innerHTML = "";
+			
+			let userId = null;
+			if (document.querySelector("#input-member-id") != null)
+				userId = document.querySelector("#input-member-id").value;
 
 			for (let vol of list) {
 
@@ -27,13 +24,13 @@ function volListLoad(url) {
 						<span class="vol_date">${vol.date}, 13:00~16:00</span>
 						<span class="vol_write_date">${vol.regdate}</span>
 							<a href="/login">
-                  <button  
-                  type="button" 
-                           data-orgVol-id="${vol.id}"
-                     data-user-id="${userId}"
-                           class="bookmark" ${vol.bookmark ? 'active' : ' '}" >
-                          </button >
-                   </a >	
+				                  <button  
+				                  type="button" 
+				                           data-orgVol-id="${vol.id}"
+				                     data-user-id="${userId}"
+				                           class="bookmark" ${vol.bookmark ? 'active' : ' '}" >
+				                          </button >
+                   				</a >	
 						<ul class="vol_list_btn_box">
 							<li><a class="vol_list_detail_btn"  href="/user/vol?id=${vol.id}">상세보기</a></li>
 						</ul>
@@ -46,58 +43,44 @@ function volListLoad(url) {
 		})
 }
 
-window.addEventListener("load", function(e) {
+/*window.addEventListener("load", function(e) {*/
 
-	let category = document.querySelector(".category");
-	let submit = document.querySelector(".submit");
-	
-	const url = new URL(window.location.href);
+let category = document.querySelector(".category");
+let submit = document.querySelector(".submit");
 
-	// URL의 쿼리 문자열을 가져옵니다.
-	const searchParams = new URLSearchParams(url.search);
+const url = new URL(window.location.href);
 
-	// 특정 매개변수의 값을 가져옵니다.
-	let cid = searchParams.get('cid');
-	
-	category.onclick = function(e) {
-		cid = e.target.value;
-		console.log(cid);
-		let word = "";
+// URL의 쿼리 문자열을 가져옵니다.
+const searchParams = new URLSearchParams(url.search);
 
-		/*클릭 이벤트가 발생한 요소의 value 속성을 가져와,해당 값을 사용하여 서버로 http 요청을 보내는 역할*/
-		volListLoad(`http://localhost:8080/api/org?c=${cid}&s=${word}`);
-	}
+// 특정 매개변수의 값을 가져옵니다.
+let cid = searchParams.get('cid');
 
-/*document.querySelector('form').addEventListener('submit', function(e) {
-		e.preventDefault(); // 기본 동작인 폼 제출을 막음
+category.onclick = function(e) {
+	cid = e.target.value;
+	/*console.log(cid);*/
+	let word = "";
 
-		// 사용자가 검색한 내용을 가져오기 위해 입력 필드를 선택하고 값(value)을 읽어옴
-		var searchInput = document.querySelector('input[name="sk"]');
-		//사용자 검색어
-		var searchValue = searchInput.value;
+	/*클릭 이벤트가 발생한 요소의 value 속성을 가져와,해당 값을 사용하여 서버로 http 요청을 보내는 역할*/
+	volListLoad(`http://localhost:8080/api/org?c=${cid}&s=${word}`);
+}
 
-		// 검색어 값을 확인하기 위해 콘솔에 출력
-		console.log('사용자가 검색한 내용:', searchValue);
 
-		// 이후에 검색어 값을 활용하여 원하는 동작을 수행
-		volListLoad(`http://localhost:8080/api/org?c=${id}&s=${searchValue}`);
-	});*/
-	
-	document.querySelector('.submit_form').addEventListener('submit', function(e) {
-    e.preventDefault(); // 폼 제출 기본 동작 막기
+document.querySelector('.submit_form').addEventListener('submit', function(e) {
+	e.preventDefault(); // 폼 제출 기본 동작 막기
 
-    var searchInput = document.querySelector('input[name="sk"]');
-    var searchValue = searchInput.value;
+	var searchInput = document.querySelector('input[name="sk"]');
+	var searchValue = searchInput.value;
 
-    // id 값을 가져오는 방식에 따라서 적절한 방법으로 id 값을 할당해주세요.
-   
+	// id 값을 가져오는 방식에 따라서 적절한 방법으로 id 값을 할당해주세요.
 
-    // 검색어 값을 확인하기 위해 콘솔에 출력
-    console.log('사용자가 검색한 내용:', searchValue);
 
-    // 검색 결과를 로드하는 함수 호출
-    volListLoad(`http://localhost:8080/api/org?c=${cid}&s=${searchValue}`);
+	// 검색어 값을 확인하기 위해 콘솔에 출력
+	console.log('사용자가 검색한 내용:', searchValue);
+
+	// 검색 결과를 로드하는 함수 호출
+	volListLoad(`http://localhost:8080/api/org?c=${cid}&s=${searchValue}`);
 });
 
-}
-);
+/*}*/
+/*);*/
